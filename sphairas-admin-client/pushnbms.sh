f4804de62b8d1d1e7af5b9d552b46c70dfd6cb8b
Boris Heithecker <boris.heithecker@gmx.net>
2020-05-02 17:23:38 +0200

    add modules update center

--- sphairas-admin-client/pushnbms.sh
+++ sphairas-admin-client/pushnbms.sh
@@ -2,9 +2,9 @@
 
 set -e
 
+TARGET=$1
 CLIENT_MODULE=sphairas-admin-client
 SOURCES=(sphairas-admin-client sphairas-admin-libs sphairas-common sphairas-libs sphairas-service)
-TARGET="gfserver@xxxxx.xxxx.xxxxxx.xxxxxx:/var/www/updates/sphairas-admin-client"
 BUILD="$CLIENT_MODULE/build/nbms"
 
 mkdir -p $BUILD
@@ -19,12 +19,14 @@ done
 TIMESTAMP=`sed 's:.* timestamp="\(.*\)".*:\1:;t;d' $BUILD/updates-$CLIENT_MODULE.xml`
 printf "TIMESTAMP: $TIMESTAMP\n"
 
-#Merge updates.xml from clusters
+#Build aggregate updates.xml from clusters
 xml_grep --pretty_print indented --wrap module_updates --descr "timestamp=\"${TIMESTAMP}\"" --cond "module" $BUILD/*.xml > $BUILD/updates.xml
 
 #Add xml declaration and DOCTYPE decl.
 sed -i -e 's:\(<?xml.*\)\(\?>\):\1encoding="UTF-8" \2:' $BUILD/updates.xml
-sed -i -e '2i<!DOCTYPE module_updates PUBLIC "-//NetBeans//DTD Autoupdate Catalog 2.6//EN" "http://www.netbeans.org/dtds/autoupdate-catalog-2_6.dtd">' $BUILD/updates.xml
+sed -i -e '2i<!DOCTYPE module_updates PUBLIC "-//NetBeans//DTD Autoupdate Catalog 2.8//EN" "http://www.netbeans.org/dtds/autoupdate-catalog-2_8.dtd">' $BUILD/updates.xml
+
+echo "Pushing nbms to ${TARGET}"
 
 #push nbm to target server
 for SOURCE in "${SOURCES[@]}"; do
