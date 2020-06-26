097a94ea5853a0505db75d1fe197659e9d43e690
Boris Heithecker <boris.heithecker@gmx.net>
2020-06-26 15:34:06 +0200

    disable unused options panel

--- sphairas-admin-libs/untis/src/org/thespheres/betula/gpuntis/package-info.java
+++ sphairas-admin-libs/untis/src/org/thespheres/betula/gpuntis/package-info.java
@@ -3,14 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-@OptionsPanelController.ContainerRegistration(id = "GPUntis",
-        categoryName = "#OptionsCategory_Name_Untis",
-        iconBase = "org/thespheres/betula/gpuntis/resources/clock.png",
-        keywords = "#OptionsCategory_Keywords_Untis",
-        keywordsCategory = "Security", 
-        position = 770)
-@NbBundle.Messages(value = {"OptionsCategory_Name_Untis=GP Untis",
-    "OptionsCategory_Keywords_Untis=untis"})
+//@OptionsPanelController.ContainerRegistration(id = "GPUntis",
+//        categoryName = "#OptionsCategory_Name_Untis",
+//        iconBase = "org/thespheres/betula/gpuntis/resources/clock.png",
+//        keywords = "#OptionsCategory_Keywords_Untis",
+//        keywordsCategory = "Security", 
+//        position = 770)
+//@NbBundle.Messages(value = {"OptionsCategory_Name_Untis=GP Untis",
+//    "OptionsCategory_Keywords_Untis=untis"})
 package org.thespheres.betula.gpuntis;
 
 import org.netbeans.spi.options.OptionsPanelController;
