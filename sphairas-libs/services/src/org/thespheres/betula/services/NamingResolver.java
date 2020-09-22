ffefe35f5648f0d00bb3e325a0c6c6fb61da2fa2
Boris Heithecker <boris.heithecker@gmx.net>
2020-09-22 18:07:26 +0200

    add unresolved hint

--- sphairas-libs/services/src/org/thespheres/betula/services/NamingResolver.java
+++ sphairas-libs/services/src/org/thespheres/betula/services/NamingResolver.java
@@ -35,7 +35,7 @@ public interface NamingResolver {
     }
 
     public Result resolveDisplayNameResult(Identity id) throws IllegalAuthorityException;
-    
+
     default public Map<String, String> properties() {
         return Collections.EMPTY_MAP;
     }
