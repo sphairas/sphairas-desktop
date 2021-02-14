aa51987467fa53d9345ae666ce9ac698ff9dd93b
Boris Heithecker <boris.heithecker@gmx.net>
2021-02-14 14:20:27 +0100

    added output writers

--- sphairas-admin-libs/xml-import/src/org/thespheres/betula/xmlimport/parse/ImportScripts.java
+++ sphairas-admin-libs/xml-import/src/org/thespheres/betula/xmlimport/parse/ImportScripts.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
 import javax.script.Invocable;
+import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
@@ -21,22 +22,25 @@ import org.openide.util.NbBundle;
 import org.openide.util.NbBundle.Messages;
 import org.thespheres.betula.document.Marker;
 import org.thespheres.betula.ui.util.PlatformUtil;
+import org.thespheres.betula.xmlimport.ImportUtil;
 
 /**
  *
  * @author boris.heithecker@gmx.net
  */
-@Messages({"ImportScripts.ScriptException.message=Exception thrown invoking funktion {0} with resolved name \"{1}\" and params {2}"})
+@Messages({"ImportScripts.ScriptException.message=Exception thrown invoking funktion {0} with resolved name \"{1}\" and params {2}: {3}"})
 public class ImportScripts {
 
     public static final String FUNCTION_PARSE_UNIT_NAME = "parseUnitName";
+    private final ScriptContext context;
     private final Invocable engine;
     private final String provider;
     private final Map<String, String> configuration = new HashMap<>();
 
-    private ImportScripts(final String provider, final Map<String, String> config, final Invocable engine) {
+    private ImportScripts(final String provider, final Map<String, String> config, final Invocable engine, final ScriptContext context) {
         this.provider = provider;
         this.engine = engine;
+        this.context = context;
         this.configuration.putAll(config);
     }
 
@@ -48,10 +52,12 @@ public class ImportScripts {
         } catch (final ScriptException ex) {
             throw new IOException(ex);
         }
-        return new ImportScripts(provider, config, (Invocable) engine);
+        return new ImportScripts(provider, config, (Invocable) engine, engine.getContext());
     }
 
     public String parseUnitName(final String resolvedName, final Marker subject, final int referenzjahr, final Integer checkLevel) {
+        context.setWriter(ImportUtil.getIO().getOut());
+        context.setErrorWriter(ImportUtil.getIO().getErr());
         final Map<String, Object> props = new HashMap<>();
         props.put("year", referenzjahr);
         if (subject != null) {
@@ -69,7 +75,7 @@ public class ImportScripts {
             final String params = props.entrySet().stream()
                     .map(e -> e.getKey() + ":" + e.getValue())
                     .collect(Collectors.joining(";", "\"", "\""));
-            final String message = NbBundle.getMessage(ImportScripts.class, "ImportScripts.ScriptException.message", new Object[]{FUNCTION_PARSE_UNIT_NAME, resolvedName, params});
+            final String message = NbBundle.getMessage(ImportScripts.class, "ImportScripts.ScriptException.message", new Object[]{FUNCTION_PARSE_UNIT_NAME, resolvedName, params, scex.getMessage()});
             PlatformUtil.getCodeNameBaseLogger(ImportScripts.class).log(Level.WARNING, message);
             return null;
         } catch (final NoSuchMethodException nsmex) {
