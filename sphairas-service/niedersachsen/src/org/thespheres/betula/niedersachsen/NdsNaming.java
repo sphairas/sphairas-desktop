1d3c346cd647e59074407c603fbd6adc76cbbb0c
Boris Heithecker <boris.heithecker@gmx.net>
2021-12-16 18:14:29 +0100

    allow unknown subjects option

--- sphairas-service/niedersachsen/src/org/thespheres/betula/niedersachsen/NdsNaming.java
+++ sphairas-service/niedersachsen/src/org/thespheres/betula/niedersachsen/NdsNaming.java
@@ -19,17 +19,20 @@ import org.thespheres.betula.services.scheme.spi.TermSchedule;
  */
 class NdsNaming extends Naming {
 
+    private final boolean permitAlternativeSubjects;
+
 //Naming
-    private NdsNaming(String provider, TermSchedule ts, String firstElement, int base, int baseAbi) {
+    private NdsNaming(String provider, TermSchedule ts, String firstElement, int base, int baseAbi, boolean permitAlternativeSubjects) {
         super(provider, ts, firstElement, base, baseAbi);
+        this.permitAlternativeSubjects = permitAlternativeSubjects;
     }
 
-    static NdsNaming create(String provider, String firstElement, int base, int baseAbi) {
+    static NdsNaming create(String provider, String firstElement, int base, int baseAbi, boolean permitAlternativeSubjects) {
         final TermSchedule ts = SchemeProvider.find(LSchB.PROVIDER_INFO.getURL()).getScheme(TermSchedule.DEFAULT_SCHEME, TermSchedule.class);
         if (ts == null) {
             throw new IllegalStateException("No TermSchedule could be found.");
         }
-        return new NdsNaming(provider, ts, firstElement, base, baseAbi);
+        return new NdsNaming(provider, ts, firstElement, base, baseAbi, permitAlternativeSubjects);
     }
 
     @Override
@@ -50,7 +53,6 @@ class NdsNaming extends Naming {
             elements.put(NdsNaming.FACH_KURZ, fach.getShortLabel());
             return true;
         } else {
-//            final String csv = LocalProperties.find(provider).getProperty(LocalFileProperties.PROP_UNIQUE_SUBJECT_CONVENTIONS, "");
             final String[] sub = type.split("\\.");
             final String id = sub[sub.length - 1];
             final String subset;
@@ -71,40 +73,10 @@ class NdsNaming extends Naming {
                 elements.put(NdsNaming.FACH_KURZ, type);
                 return true;
             }
-            //            for(String t : csv.split(",")) {
-//                MarkerConvention mc = MarkerFactory.findConvention(t);
-//                if(mc != null) {
-//                    final String dn = mc.getDisplayName();
-//                    if(cnvname == null || dn.equalsIgnoreCase(cnvname)) {
-//                        Marker mf = mc.find(id, subset);
-//                        if(mf != null) {
-//                            if(fach != null) {
-//                                return false;
-//                            }
-//                            fach = mf;
-//                        }
-//                    }
-//                }
-//            }
-//            final List<Marker> mm = Arrays.stream(csv.split(","))
-//                    .map(MarkerFactory::findConvention)
-//                    .filter(Objects::nonNull)
-//                    .filter(cv -> cnvname == null || cv.getDisplayName().equalsIgnoreCase(cnvname))
-//                    .map(cv -> cv.find(id, subset))
-//                    .filter(Objects::nonNull)
-//                    .collect(Collectors.toList());
-//            final String uniqueLongLabel = mm.stream()
-//                    .map(m -> m.getLongLabel())
-//                    .distinct()
-//                    .collect(CollectionUtil.singleOrNull());
-//            final String uniqueShortLabel = mm.stream()
-//                    .map(m -> m.getShortLabel())
-//                    .distinct()
-//                    .collect(CollectionUtil.singleOrNull());
-//            if (uniqueLongLabel != null && uniqueShortLabel != null) {
-//
-//                return true;
-//            }
+            if (permitAlternativeSubjects) {
+                elements.put(NdsNaming.FACH, StringUtils.capitalize(type));
+                return true;
+            }
         }
         return false;
     }
