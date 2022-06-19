9262e1f3b956e1a5bfe2ce11fdd39b84f7fa57dd
Boris Heithecker <boris.heithecker@gmx.net>
2022-06-19 17:18:28 +0200

    Add neue Zeile tag

--- sphairas-service/niedersachsen/src/org/thespheres/betula/niedersachsen/zeugnis/Constants.java
+++ sphairas-service/niedersachsen/src/org/thespheres/betula/niedersachsen/zeugnis/Constants.java
@@ -15,6 +15,7 @@ import org.thespheres.betula.tag.AbstractTag;
 public class Constants {
 
     public static final Tag SCHLUSS_BEMERKUNG = new AbstractTag("org.thespheres.betula.niedersachsen.zeugnis.bemerkungen", "schluss");
+    public static final Tag NEUE_ZEILE = new AbstractTag("org.thespheres.betula.niedersachsen.zeugnis.bemerkungen", "neue.zeile");
     public static String CATEGORY_ZEUGNISAUSGABE = "zeugnisausgabe";
     public static String CATEGORY_ZEUGNISKONFERENZ = "zeugniskonferenz";
     public static String ABSCHLUSSZEUGNISSE = "abschlusszeugnisse";
