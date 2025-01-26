dbd86882ffac2443fb035f31d8a3496f9f3bb420
Boris Heithecker <boris.heithecker@gmx.net>
2025-01-26 23:49:25 +0100

    Throw exception if calendar resource file contains more than one calendar.

--- sphairas-service/niedersachsen/src/org/thespheres/betula/niedersachsen/zeugnis/ZeugnisDatenCalendarProvider.java
+++ sphairas-service/niedersachsen/src/org/thespheres/betula/niedersachsen/zeugnis/ZeugnisDatenCalendarProvider.java
@@ -8,6 +8,7 @@ package org.thespheres.betula.niedersachsen.zeugnis;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.ParseException;
+import java.util.List;
 import org.openide.util.lookup.ServiceProvider;
 import org.thespheres.betula.niedersachsen.LSchB;
 import org.thespheres.betula.services.ProviderInfo;
@@ -24,13 +25,18 @@ import org.thespheres.ical.builder.ICalendarBuilder;
 @ServiceProvider(service = CalendarResourceProvider.class)
 public class ZeugnisDatenCalendarProvider implements CalendarResourceProvider {
 
+    static final String FILE = "/org/thespheres/betula/niedersachsen/resources/ausgabetermine.ics";
     private ICalendar calendar;
     private final static CalendarResourceType TYPE = new CalendarResourceType("zeugnisausgabe") {
     };
 
     public ZeugnisDatenCalendarProvider() throws IOException {
-        try (InputStream is = ZeugnisDatenCalendarProvider.class.getResourceAsStream("/org/thespheres/betula/niedersachsen/resources/ausgabetermine.ics")) {
-            calendar = ICalendarBuilder.parseCalendars(is).get(0);
+        try (InputStream is = ZeugnisDatenCalendarProvider.class.getResourceAsStream(FILE)) {
+            final List<ICalendar> p = ICalendarBuilder.parseCalendars(is);
+            if (p.size() != 1) {
+                throw new IllegalStateException("Resource " + FILE + " must contain exactly one calender.");
+            }
+            calendar = p.get(0);
         } catch (ParseException | InvalidComponentException ex) {
             throw new IOException(ex);
         }
